00be8aed54b0ff26dd59537fdab47832c3a0def7
Jurason <evgenilymanyuk@gmail.com>
2022-05-23 19:58:11 +0300

    PageObjects

---
+++ src/main/java/PageObjects/firstClass.java
@@ -0,0 +1,30 @@
+package PageObjects;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class firstClass {
+
+    public static  void main (String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\djon\\IdeaProjects\\tools\\chromedriver.exe");
+        ChromeDriver browserSession = new ChromeDriver();
+        browserSession.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(browserSession, Duration.ofSeconds(10));
+
+        try {
+
+            browserSession.get("");
+            Thread.sleep(5000);
+
+            LoginPage lp = new LoginPage();
+            lp.login("admin", "admin");
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            browserSession.quit();
+        }
+    }
+}
+
